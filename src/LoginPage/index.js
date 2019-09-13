035103692ef920c50e150eeae664cb43864a7793
Jack Stobbe <jjstobbe@gmail.com>
2019-09-13 01:24:38 -0500

    Register page scaffolding, adding state and error management

--- src/LoginPage/index.js
+++ src/LoginPage/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import * as styles from './styles.module.scss';
 
 export function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [hasError, setHasError] = useState(false);
+
+  const setEmailEvent = (e) => {
+    setEmail(e.target.value);
+  }
+  const setPasswordEvent = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      return;
+    }
+
+    setEmail('');
+    setPassword('');
+    setHasError(true);
+  }
+
   return (
     <div className={styles.mainContent}>
-      <form>
+      <form onSubmit={submit}>
         <div className={styles.loginMessage}>
           <label>Sign in to Kanban</label>
           <span></span>
@@ -18,15 +41,17 @@ export function LoginPage() {
         </div>
         
         <div className={styles.textInput}>
-          <input type="text" placeholder="Email" />
+          <input type="email" placeholder="Email" value={email} onChange={setEmailEvent} required />
           <span></span>
         </div>
         <div className={styles.textInput}>
-          <input type="text" placeholder="Password" />
+          <input type="password" placeholder="Password" value={password} onChange={setPasswordEvent} required />
           <span></span>
         </div>
+
+        {hasError && <div className={styles.loginError}>Email or Password is not valid</div>}
         
-        <button>
+        <button type="submit">
           Sign in
         </button>
       </form>
--- src/RegistrationPage/index.js
+++ src/RegistrationPage/index.js
@@ -1,10 +1,75 @@
-import React from 'react';
-// import * as styles from './styles';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
+import * as styles from './styles.module.scss';
 
 export function RegistrationPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [hasError, setHasError] = useState(false);
+
+  const setEmailEvent = (e) => {
+    setEmail(e.target.value);
+  }
+  const setPasswordEvent = (e) => {
+    setPassword(e.target.value);
+  }
+  const setConfirmPasswordEvent = (e) => {
+    setConfirmPassword(e.target.value);
+  }
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    if (!email || !password || !confirmPassword) {
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setHasError(true);
+      setConfirmPassword('');
+      return;
+    }
+
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+    setHasError(false);
+  }
+
   return (
-    <div>
-      Register
+    <div className={styles.mainContent}>
+      <form onSubmit={submit}>
+        <div className={styles.loginMessage}>
+          <label>Create an Account</label>
+          <span></span>
+        </div>
+
+        <div className={styles.createAccount}>
+          <label>
+            Already have an account? <Link to="login">Login</Link>
+          </label>
+        </div>
+        
+        <div className={styles.textInput}>
+          <input type="email" placeholder="Email" value={email} onChange={setEmailEvent} required />
+          <span></span>
+        </div>
+        <div className={styles.textInput}>
+          <input type="password" placeholder="Password" value={password} onChange={setPasswordEvent} required />
+          <span></span>
+        </div>
+        <div className={styles.textInput}>
+          <input type="password" placeholder="Confim Password" value={confirmPassword} onChange={setConfirmPasswordEvent} required />
+          {hasError ? 
+            <div className={styles.inputErrorMessage}>Those passwords didn't match</div> :
+            <span></span>}
+        </div>
+        
+        <button type="submit">
+          Create
+        </button>
+      </form>
     </div>
   );
 }
