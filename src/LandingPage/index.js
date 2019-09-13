414cbe5b63182f54eac5ff58cbadb15f33575ee7
Jack Stobbe <jjstobbe@gmail.com>
2019-09-12 23:21:55 -0500

    Basic Login Page

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -1,31 +1,29 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 import * as styles from './styles.module.scss';
 
 export function LandingPage() {
   return (
-    <Fragment>
-      <div className={styles.mainContent}>
+    <div className={styles.mainContent}>
 
-        <div className={styles.welcome}>
-          <span className={styles.mainHeader}>
-            Kanban
-          </span>
-          <span className={styles.subheader}>
-            Sign in to manage your day.
-          </span>
-        </div>
-
-        <div className={styles.links}>
-          <Link to="/login">
-            <button>Login</button>
-          </Link>
-          <Link to="/register">
-            <button>Register</button>
-          </Link>
-        </div>
-
+      <div className={styles.welcome}>
+        <span className={styles.mainHeader}>
+          Kanban
+        </span>
+        <span className={styles.subheader}>
+          Sign in to manage your day.
+        </span>
       </div>
-    </Fragment>
+
+      <div className={styles.links}>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
+      </div>
+
+    </div>
   );
 }
--- src/LoginPage/index.js
+++ src/LoginPage/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
-// import * as styles from './styles';
+import { Link } from 'react-router-dom'
+import * as styles from './styles.module.scss';
 
 export function LoginPage() {
   return (
-    <div>
-      Login
+    <div className={styles.mainContent}>
+      <form>
+        <div className={styles.loginMessage}>
+          <label>Sign in to Kanban</label>
+          <span></span>
+        </div>
+
+        <div className={styles.createAccount}>
+          <label>
+            Or <Link to="register">Create Account</Link>
+          </label>
+        </div>
+        
+        <div className={styles.textInput}>
+          <input type="text" placeholder="Email" />
+          <span></span>
+        </div>
+        <div className={styles.textInput}>
+          <input type="text" placeholder="Password" />
+          <span></span>
+        </div>
+        
+        <button>
+          Sign in
+        </button>
+      </form>
     </div>
   );
 }
