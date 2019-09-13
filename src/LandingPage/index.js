9ff50c2e095209b493ac06344841a2388fd3f83e
Jack Stobbe <jjstobbe@gmail.com>
2019-09-12 22:45:03 -0500

    Basic landing page

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
-// import * as styles from './styles';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom'
+import * as styles from './styles.module.scss';
 
 export function LandingPage() {
   return (
-    <div>
-      Welcome
-    </div>
+    <Fragment>
+      <div className={styles.mainContent}>
+
+        <div className={styles.welcome}>
+          <span className={styles.mainHeader}>
+            Kanban
+          </span>
+          <span className={styles.subheader}>
+            Sign in to manage your day.
+          </span>
+        </div>
+
+        <div className={styles.links}>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+
+      </div>
+    </Fragment>
   );
 }
